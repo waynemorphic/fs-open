d64a8a4b55e3f9ce8767f906f5fa40a35e4aa414
waynemorphic <wayneklyde@gmail.com>
2023-03-01 22:42:20 +0300

    alert if name exists in phonebook

--- part2/phonebook/App.jsx
+++ part2/phonebook/App.jsx
@@ -1,81 +1,69 @@
 import "./styles.css";
-import { useState } from 'react'
+import { useState } from "react";
 
-// component to filter person by name
-const Filter = ({searcher, text}) => {
-  return(
-    <div>
-      filter shown with <input value = {text} onChange = {searcher} />
-    </div>
-  )
-}
 export default function App() {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: '123-456-789'}
-  ])
-  // console.log("persons name ", persons)
-  const [newName, setNewName] = useState('Initial state of name...')
-  const [newNumber, setNewNumber] = useState('123-111-111')
+    { name: "Arto", number: "123-456-789" },
+    { name: "James", number: "112-333-444" }
+  ]);
+  const [newName, setNewName] = useState("Initial state of name...");
+  const [newNumber, setNewNumber] = useState("123-111-111");
+  const [nameCheck, setNameCheck] = useState([]);
 
-  // filter the names in phonebook by name
-  const filterPhonebook = (event) => {
-    setPersons(event.target.value)
-    console.log(event.target.value)
-    // console.log("filter method", persons.filter((person) => persons.name === person.name ))
-  }
-  
   // event handler to synchronize to new state after adding new name
   // preventDefault() is not called as no default action occurs on input change
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
-  // form submission button event handler
-  const addPerson = (event) => {
-    event.preventDefault()
+  // form submission event handler. Prevents default html form submission
+  const handlePersonAddition = (event) => {
+    event.preventDefault();
 
     // new object to add person
     const personObject = {
       name: newName,
       number: newNumber
-    }
-    setPersons(persons.concat(personObject))
-    
-    // console.log(personObject.name)
-    setNewName('')
-     // add an alert if name of person added exists    
-  }
+    };
+
+    // check if name exists in the phonebook
+    persons.filter((person) => {
+      person.name.toLowerCase() === personObject.name.toLowerCase()
+        ? alert(personObject.name + " already exists in phonebook")
+        : event.target.reset();
+      return person.name;
+    });
+
+    // add name and number on UI
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter searcher = {filterPhonebook} text = {newName} />
-      <form onSubmit = {addPerson}>
+      {/* <Filter searcher = {filterPhonebook} text = {newName} /> */}
+      <h2>Add a new</h2>
+      <form onSubmit={handlePersonAddition}>
         <div>
-          name: <input 
-                  value = {newName} 
-                  onChange = {handleNameChange}
-                  />
-                <br></br>
-          number: <input
-                    value = {newNumber}
-                    onChange = {handleNumberChange}
-                  />
+          name: <input value={newName} onChange={handleNameChange} />
+          <br></br>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
-          <button type = 'submit'>add</button>
-        </div> 
+          <button type="submit">add</button>
+        </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(putName => 
-        <ol key = {putName.id}>
+      {persons.map((putName) => (
+        <ol key={putName.uniqueId}>
           {putName.name} : {putName.number}
         </ol>
-      )}
+      ))}
     </div>
   );
 }
