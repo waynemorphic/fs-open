0766ecb3474cba49b59fec6f6360a6ac1b917039
waynemorphic <wayneklyde@gmail.com>
2023-03-02 12:58:10 +0300

    pop up with alert

--- part2/phonebook/App.jsx
+++ part2/phonebook/App.jsx
@@ -1,14 +1,55 @@
-import "./styles.css";
 import { useState } from "react";
+
+const Filter = (props) => {
+  // console.log(...props.persons.filter((person) => person.name))
+  return(
+    <>
+      <div>
+      filter shown with: <input  {...props.persons.filter((person) => person.name)}/>
+      </div>
+    </>
+  )
+}
+
+// form to add a person's name and number
+const PersonForm = ({eventPerson, eventName, eventNumber, stateName, stateNumber}) => {
+  return(
+    <>
+      <form onSubmit={eventPerson}>
+          <div>
+            name: <input value={stateName} onChange={eventName} />
+            <br></br>
+            number: <input value={stateNumber} onChange={eventNumber} />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+    </>
+  )
+}
+
+// Persons component
+const Persons = ({persons}) => { 
+   
+  return(
+    <>
+      {persons.map((person) => (
+        <li key = {persons.indexOf(person)}>
+          {person.name}: {person.number}
+        </li>
+        ))}
+    </>
+  )
+}
 
 export default function App() {
   const [persons, setPersons] = useState([
     { name: "Arto", number: "123-456-789" },
     { name: "James", number: "112-333-444" }
   ]);
-  const [newName, setNewName] = useState("Initial state of name...");
+  const [newName, setNewName] = useState("mark");
   const [newNumber, setNewNumber] = useState("123-111-111");
-  const [nameCheck, setNameCheck] = useState([]);
 
   // event handler to synchronize to new state after adding new name
   // preventDefault() is not called as no default action occurs on input change
@@ -25,25 +66,55 @@ export default function App() {
     event.preventDefault();
 
     // new object to add person
-    const personObject = {
-      name: newName,
-      number: newNumber
-    };
+    // const personObject = {
+    //   name: newName,
+    //   number: newNumber,
+    // };
+    
 
-    // loop and condition to check if name exists in phonebook
+    // We are checking if a name exists in the array
+    let j, k;
     for (let i = 0; i < persons.length; i++) {
-      console.log("persons", persons[i].name);
-      if (persons[i].name.toLowerCase() === personObject.name.toLowerCase()) {
-        const index = persons.indexOf(persons[i].name);
-        console.log(persons.splice(index, 1));
-        console.log(persons);
-        event.target.reset();
-        alert(personObject.name + " already exists in phonebook");
-      } else {
+      // returns a boolean value
+      const personObject = {
+        name: newName,
+        number: newNumber,
+      };
+      k = persons[i].name.toLowerCase() === personObject.name.toLowerCase()
+      // j = persons.map((i) => i.name).includes(k);
+      // console.log("existing?", j)
+      if (k === true){
+        // console.log("value of k",k)
+        alert(personObject.name + " name exists in phonebook")
+      }
+      else {
+        event.target.reset()
+        console.log(personObject)
         setPersons(persons.concat(personObject));
         setNewName("");
       }
     }
+    
+  // console.log("jajaj",persons.filter((person) => {
+  //   person.name === j
+  // ? alert(person.name, "name exists in phonebook")
+  // : {}
+  // }))
+
+    // loop and condition to check if name exists in phonebook
+    // for (let i = 0; i < persons.length; i++) {
+    //   console.log("persons", persons[i].name);
+    //   if (persons[i].name.toLowerCase() === personObject.name.toLowerCase()) {
+    //     const index = persons.indexOf(persons[i].name);
+    //     console.log(persons.splice(index, 1));
+    //     console.log(persons);
+    //     event.target.reset();
+    //     alert(personObject.name + " already exists in phonebook");
+    //   } else {
+    //     setPersons(persons.concat(personObject));
+    //     setNewName("");
+    //   }
+    // }
     // check if name exists in the phonebook
     // persons.filter((person) => {
     //   person.name.toLowerCase() === personObject.name.toLowerCase()
@@ -57,23 +128,17 @@ export default function App() {
     <div>
       <h2>Phonebook</h2>
       {/* <Filter searcher = {filterPhonebook} text = {newName} /> */}
+      <Filter stateName = {newName} eventName = {handleNameChange} persons = {persons} />
       <h2>Add a new</h2>
-      <form onSubmit={handlePersonAddition}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-          <br></br>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm 
+        eventPerson = {handlePersonAddition} 
+        stateName = {newName} 
+        stateNumber = {newNumber}
+        eventName = {handleNameChange}
+        eventNumber = {handleNumberChange} 
+      />
       <h2>Numbers</h2>
-      {persons.map((putName) => (
-        <ol key={putName.uniqueId}>
-          {putName.name} : {putName.number}
-        </ol>
-      ))}
+      <Persons persons = {persons}/>
     </div>
   );
 }
